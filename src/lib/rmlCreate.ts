33079a23a7de8cff16bba728e1c829d26da5d907
Matteo D'Urso <67522359+MatteoDrso@users.noreply.github.com>
2026-06-24 17:16:21 +0200

    Added wrapping YARRRML Doc and wrote out method stubs

--- src/lib/rmlCreate.ts
+++ src/lib/rmlCreate.ts
@@ -1,38 +1,88 @@
-import { Document } from 'yaml'
+import { stringify } from 'yaml'
 import type { SubjectType, PredicateType, ObjectType } from './rmlExport';
+import { NS } from './rdfVocabulary';
+
+interface YARRRMLMapping {
+  sources: string[];
+  s: string;
+  po: (string | [string, string] | [string, string, string] | { p: string; o: { value: string; type?: string; datatype?: string } })[];
+}
+
+interface YARRRMLDocument {
+  prefixes: Record<string, string>;
+  mappings: Record<string, YARRRMLMapping>;
+}
 
 export class RMLDocument {
-  private doc: Document;
+  private doc: YARRRMLDocument;
+  private dataSource: string = '';
   
-  constructor(prefixes: Map<string, string> = undefined, dataSource: string = undefined) {
-    
-    this.doc = new Document([
-      { mapping: 
-        { data: undefined } 
-      }
-    ]);
-
-    this.setPrefixes(prefixes)
-    this.setDataSource(dataSource)
+  constructor() {
+    this.doc = {
+      prefixes: {
+        rdf: NS.rdf,
+        rdfs: NS.rdfs,
+        owl: NS.owl,
+        xsd: NS.xsd,
+        ex: 'http://example.org/',
+      },
+      mappings: {},
+    };
   }
-  
-  addSubject(subject: SubjectType): this { 
+
+  setDataSource(fileName: string): this {
+    this.dataSource = fileName;
     return this;
   }
 
-  addPredicateObject(predicate: PredicateType, object: ObjectType): this { 
+  addPrefix(prefix: string, uri: string): this {
+    this.doc.prefixes[prefix] = uri;
     return this;
   }
 
-  setDataSource(file_name: string): this {
-    (this.doc.getIn(["mapping", "data"]) as Document).set("sources", file_name + "~" + file_name.split(".").pop());
-    return this;
-  } 
+  addSubject(subject: SubjectType): this {
+    const mappingKey = this.generateMappingKey(subject.classUri);
 
-  setPrefixes(prefixes: Map<string, string>) {
-    this.doc.set("prefixes", prefixes)
+    if (!this.doc.mappings[mappingKey]) {
+      const sourceRef = this.dataSource.endsWith('.csv')
+        ? `${this.dataSource}~csv`
+        : this.dataSource;
+
+      this.doc.mappings[mappingKey] = {
+        sources: [sourceRef],
+        s: subject.template,
+        po: [
+          ['a', subject.classUri + '~iri'],
+        ],
+      };
+    }
+
+    return this;
   }
 
-  serialize(): string { return this.doc.toString(); }
+  addPredicateObject(predicate: PredicateType, object: ObjectType, classUri: string): this {
+    const mappingKey = this.generateMappingKey(classUri);
+    const mapping = this.doc.mappings[mappingKey];
 
+    if (!mapping) {
+      return this;
+    }
+
+    if (object.datatype) {
+      mapping.po.push([predicate.uri, `$(${object.column})`, object.datatype]);
+    } else {
+      mapping.po.push([predicate.uri, `$(${object.column})`]);
+    }
+
+    return this;
+  }
+
+  private generateMappingKey(classUri: string): string {
+    const shortName = classUri.split(/[#/]/).pop() || classUri;
+    return shortName.toLowerCase().replace(/[^a-z0-9]/g, '_');
+  }
+
+  serialize(): string {
+    return stringify(this.doc, { lineWidth: 0 });
+  }
 }
